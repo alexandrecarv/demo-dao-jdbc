b29f7574ded22ee294fbb86081cff06c7f621503
Alexandre Carvalho <falexandreco@gmail.com>
2022-06-27 16:49:37 -0300

    DepartmentDao implementation and test

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -17,6 +17,10 @@ public class Department implements Serializable{
 		this.id = id;
 		this.name = name;
 	}
+	
+	public Department(String name) {
+		this.name = name;
+	}
 
 	public int getId() {
 		return id;
